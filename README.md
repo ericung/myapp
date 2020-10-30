ca2f6101cf1f36c1ff086ad14c15b041ec96d7a6
Eric Ung <ungericw@tutanota.com>
2020-10-30 13:02:14 -0500

    work on initial template

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-Web Application that I will build to template future projects.
+# Web Application that I will build to template future projects.
+
+## Building a single binary containing templates
+
+This is a complete example to create a single binary with the
+[gin-gonic/gin][gin] Web Server with HTML templates.
+
+[gin]: https://github.com/gin-gonic/gin
+
+## How to use
+
+### Prepare Packages
+
+```
+go get github.com/gin-gonic/gin
+go get github.com/jessevdk/go-assets-builder
+```
+
+### Generate assets.go
+
+```
+go-assets-builder html -o assets.go
+```
+
+### Build the server
+
+```
+go build -o assets-in-binary
+```
+
+### Run
+
+```
+./assets-in-binary
+```
--- main.go
+++ main.go
@@ -1,25 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
-
-	router.GET("/cookie", func(c *gin.Context) {
-
-		cookie, err := c.Cookie("gin_cookie")
-
-		if err != nil {
-			cookie = "NotSet"
-			c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
-		}
-
-		fmt.Printf("Cookie value: %s \n", cookie)
+	r := gin.New()
+	t, err := loadTemplate()
+	if err != nil {
+		panic(err)
+	}
+	r.SetHTMLTemplate(t)
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "/html/index.tmpl", gin.H{
+			"Foo": time.Date(2017, 07, 01, 0, 0, 0, 0, time.UTC),
+		})
 	})
+	r.GET("/bar", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "/html/bar.tmpl", gin.H{
+			"Bar": "World",
+		})
+	})
+	r.Run("localhost:9000")
+}
 
-	router.Run("localhost:9000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+func loadTemplate() (*template.Template, error) {
+	t := template.New("")
+	for name, file := range Assets.Files {
+		if file.IsDir() || !strings.HasSuffix(name, ".tmpl") {
+			continue
+		}
+		h, err := ioutil.ReadAll(file)
+		if err != nil {
+			return nil, err
+		}
+		t, err = t.New(name).Parse(string(h))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return t, nil
 }
