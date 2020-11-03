64f422f465d98c84cf6c399f9dcf6e97957b5c1c
Eric Ung <ungericw@tutanota.com>
2020-11-03 11:35:53 -0600

    11/3/2020 database connection and testing

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+
+	"gorm.io/gorm"
 )
+
+type User struct {
+	id    int
+	name  string
+	email string
+}
 
 func main() {
 	r := gin.New()
@@ -21,8 +30,31 @@ func main() {
 	r.Static("/wwwroot/js", "./wwwroot/js")
 
 	r.GET("/", func(c *gin.Context) {
+		content, err := ioutil.ReadFile("database.config")
+		if err != nil {
+			fmt.Println(err)
+		}
+		dsn := string(content)
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		fmt.Println("Connecting")
+		if err != nil {
+			fmt.Println("Error")
+			return
+		}
+
+		/*
+			user := map[string]interface{}{}
+			user["id"] = 12
+			user["name"] = "bill"
+			user["email"] = "bill@bill.com"
+			db.Table("user").Create(&user)
+		*/
+
+		result := map[string]interface{}{}
+		db.Table("user").Take(&result)
+
 		c.HTML(http.StatusOK, "/views/index.tmpl", gin.H{
-			"Foo": time.Now(),
+			"Foo": result["id"],
 		})
 	})
 
